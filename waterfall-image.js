2550599b908dd544b3a661a2457dbc9be2cb5fcb
Siddhant Shukla <70441430+Siddhantshukla814@users.noreply.github.com>
2022-03-15 01:50:44 +0530

    Recipe added for waterfall image

---
+++ waterfall-image.js
@@ -0,0 +1,30 @@
+const WebPageTest = require("webpagetest");
+const fs = require("fs");
+const axios = require("axios");
+
+const wpt = new WebPageTest("https://www.webpagetest.org", "YOUR_API_KEY");
+
+let testURL = "https://docs.webpagetest.org/"; //Your URL here
+
+let options = {
+  firstViewOnly: true,
+  location: "Dulles:Chrome",
+  connectivity: "4G",
+  pollResults: 5, //keep polling for results after test is scheduled
+};
+
+wpt.runTest(testURL, options, (err, result) => {
+  if (result) {
+    let imgurl = result.data.median.firstView.images.waterfall;
+
+    axios({
+      method: "get",
+      url: imgurl,
+      responseType: "stream",
+    }).then(function (response) {
+      response.data.pipe(fs.createWriteStream("waterfall.png"));
+    });
+  } else {
+    console.log(err);
+  }
+});
