b0d393f64b64a4a5ee6b8371fc4ee9d0a3fd0472
Siddhant Shukla <70441430+Siddhantshukla814@users.noreply.github.com>
2022-04-28 21:51:22 +0530

    Recipe update with better code

--- webvitals.js
+++ webvitals.js
@@ -2,13 +2,6 @@ const WebPageTest = require("webpagetest");
 
 const wpt = new WebPageTest("https://www.webpagetest.org", "YOUR_API_KEY");
 
-// CoreWebVitals
-keys = [
-  "chromeUserTiming.CumulativeLayoutShift",
-  "chromeUserTiming.LargestContentfulPaint",
-  "TotalBlockingTime",
-];
-
 let testURL = "https://docs.webpagetest.org/"; //Your URL here
 
 let options = {
@@ -20,14 +13,11 @@ let options = {
 
 wpt.runTest(testURL, options, (err, result) => {
   if (result) {
-    const data = keys.reduce(
-      (key, value) => ({
-        ...key,
-        [value]: result.data.average.firstView[value],
-      }),
-      {}
-    );
-    console.log(data);
+    console.log({
+      CumulativeLayoutShift: result.data.average.firstView["chromeUserTiming.CumulativeLayoutShift"],
+      LargestContentfulPaint: result.data.average.firstView["chromeUserTiming.LargestContentfulPaint"],
+      TotalBlockingTime: result.data.average.firstView["TotalBlockingTime"],
+    });
   } else {
     console.log(err);
   }
